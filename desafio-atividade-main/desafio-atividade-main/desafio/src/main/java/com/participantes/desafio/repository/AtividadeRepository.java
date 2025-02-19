8e701a42b1612cc279a9035f6fd25c05166efaf6
Matthz-max <matheus7serafim@gmail.com>
2025-02-18 22:33:14 -0300

    Delete desafio-atividade-main/desafio-atividade-main/desafio directory

--- desafio-atividade-main/desafio-atividade-main/desafio/src/main/java/com/participantes/desafio/repository/AtividadeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.participantes.desafio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.participantes.desafio.entities.Atividade;
-
-public interface AtividadeRepository extends JpaRepository<Atividade, Integer> {
-
-}
